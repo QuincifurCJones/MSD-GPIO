c67278e205bbd68bf2152e9fe91502b4995d1483
QuincifurCJones <qgerhardstein@stny.rr.com>
2023-03-09 14:02:08 -0500

    Adding innitial files to test
    - Servo file
    -Ultra sonic sensors

---
+++ UltraSonic_test.py
@@ -0,0 +1,39 @@
+#install
+import RPi.GPIO as GPIO
+import time
+
+#code
+GPIO.setmode(GPIO.BCM)
+
+TRIG = 23
+ECHO = 24
+
+print("Distance Measurement In Progress")
+
+GPIO.setup(TRIG, GPIO.OUT)
+GPIO.setup(ECHO, GPIO.IN)
+
+try:
+    while True:
+        GPIO.output(TRIG, False)
+        print("waiting for sensor to settle")
+        time.sleep(2)
+
+        GPIO.output(TRIG, True)
+        time.sleep(0.00001)
+        GPIO.output(TRIG, False)
+
+        while GPIO.input(ECHO) == 0:
+            pulse_start = time.time()
+        while GPIO.input(ECHO) == 1:
+            pulse_end = time.time()
+
+        pulse_duration = pulse_end - pulse_start
+
+        distance = pulse_duration * 17150
+        distance = round(distance, 2)
+        print("Distance: ", distance, "cm")
+
+except KeyboardInterrupt: # If there is a KeyboardInterrupt (when you press ctrl+c), exit the program
+    print("Cleanign Up!")
+    GPIO.cleanup()
